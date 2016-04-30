442a055800d705530474a329b4e4d2b9aebbe821
Nekyia <matthieujullien42@gmail.com>
2016-04-30 21:51:22 +0200

    Update llist_merge.c

--- llist_merge.c
+++ llist_merge.c
@@ -3,7 +3,7 @@
 void    llist_merge(t_llist *list1, t_llist *list2)
 {
     if (list1 == NULL || list2 == NULL)
-        return (NULL);
+        return ;
     list1->last->next = list2->first;
     list1->last = list2->last;
     list1->size = list1->size + list2->size;
