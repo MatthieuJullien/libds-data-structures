f29f3d3b15d5c9ed93135e81d1667c347a0a8637
Nekyia <matthieujullien42@gmail.com>
2016-04-30 21:40:52 +0200

    Update llist_merge.c

--- llist_merge.c
+++ llist_merge.c
@@ -1,11 +1,10 @@
 #include "libds.h"
 
-t_llist     *llist_merge(t_llist *list1, t_llist *list2)
+void    llist_merge(t_llist *list1, t_llist *list2)
 {
     if (list1 == NULL || list2 == NULL)
         return (NULL);
     list1->last->next = list2->first;
     list1->last = list2->last;
     list1->size = list1->size + list2->size;
-    return (list1);
 }
